728a40a2f05dd1c745eb681f3fbf15cce360fa40
michael-etzkorn <michael.etzkorn@sage-micro.com>
2021-09-19 13:54:23 -0500

    Removing rst checking because it's not necessary and its potentially buggy

--- ChiselWrapper.py
+++ ChiselWrapper.py
@@ -6,22 +6,17 @@ def main():
     fname = ""                         # Insert your file name here TODO: move this to command line
     vlog_ex = vlog.VerilogExtractor()
     vlog_mods = vlog_ex.extract_objects(fname)
-    given_class_name = ""              # TODO: Add CLI parser
     fname_noext = fname.split(".")[0]  # assumes file name does not contain "." outside of ".v" or ".sv"
-    # given_class_name = pass
-    if given_class_name == "":
-        given_class_name = fname_noext 
-    
     mod = vlog_mods[0]                 # assumes file contains only a top level module (unclear on how Chisel's Blackbox API handles multiple modules in top)
 
-    with open(f"{given_class_name}.scala", "w") as f: # TODO: allow naming of output .scala file from command line with given_class_name
+    with open(f"{fname_noext}.scala", "w") as f:
         f.write("// package ReplaceMe\n")
         f.write("import chisel3._ \n")
         f.write("import chisel3.util._ \n")
         f.write("import chisel3.experimental.{IntParam, BaseModule}\n")
 
         if mod.generics: # Parameters exist
-            f.write(f"case class {given_class_name}Params(\n")
+            f.write(f"case class {fname_noext}Params(\n")
             for param in mod.generics: # TODO: A conversion of snake case to camel case for param names would be good for the Scala syntax tool, but shouldn't affect compilation
                 param_scaltype = "Int" 
                 use_x = ""
@@ -42,7 +37,7 @@ def main():
 
                 
 
-            f.write(f"class {given_class_name}BlackBox(val c: {given_class_name}Params) extends BlackBox(Map(\n") # Probably a way to inline a conditional concatenation of (val c: NameParams)
+            f.write(f"class {fname_noext}(val c: {fname_noext}Params) extends BlackBox(Map(\n") # Probably a way to inline a conditional concatenation of (val c: NameParams)
             
             for param in mod.generics: 
                 comma = ","
@@ -51,8 +46,8 @@ def main():
                 f.write(f"\t\"{param.name}\" -> IntParam(c.{param.name}){comma}\n") # NOTE: if we convert name from snake_case to camelCase, this line needs to change
             f.write("))")
         else:  
-            f.write(f"class {given_class_name}BlackBox extends BlackBox")
-        f.write(f" with HasBlackBoxResource with Has{given_class_name}IO\n")
+            f.write(f"class {fname_noext} extends BlackBox")
+        f.write(f" with HasBlackBoxResource with Has{fname_noext}IO\n")
         f.write("{\n")
 
          # TODO: Refactor to allow a directory to be given containing all files to be included with option of placing under directory name
@@ -62,9 +57,9 @@ def main():
 
         # Process ports
         if mod.generics:
-            f.write(f"class {given_class_name}(val c: {given_class_name}Params) extends Bundle{{\n")
+            f.write(f"class {fname_noext}(val c: {fname_noext}Params) extends Bundle{{\n")
         else: 
-            f.write(f"class {given_class_name} extends Bundle{{\n")
+            f.write(f"class {fname_noext} extends Bundle{{\n")
         
         # NOTE: The snake_case to camelCase idea is tricky here. We need to parse for param names within the data_type then call a function to convert to camelCase. 
         # WARNING: This assumes clock and resets have clk and rst in their name. They don't always. Double check input file and output file for correct clock reset names.
@@ -78,8 +73,6 @@ def main():
                 direction = "Analog" # I assume "inout" is the only other type
             if "clk" in port.name:
                 f.write(f"\t val {port.name} = {direction}(Clock())\n")
-            elif "rst" in port.name and "burst" not in port.name:  # WARNING: there are 490+ words in the English language that contain 'rst' ... 
-                f.write(f"\t val {port.name} = {direction}(Bool())\n")
             else:
                 # process datatype
                 # TODO: add multidimension port support
@@ -105,12 +98,12 @@ def main():
                 else: 
                     f.write(f"\t val {port.name} = {direction}(UInt({abs(int(msb) - int(lsb)) + 1}.W))\n")
         f.write("\n}\n")
-        f.write(f"trait Has{given_class_name}IO extends BaseModule {{\n")
+        f.write(f"trait Has{fname_noext}IO extends BaseModule {{\n")
         pass_arg = ""
         if mod.generics:
-            f.write(f"\tval c: {given_class_name}Params\n")
+            f.write(f"\tval c: {fname_noext}Params\n")
             pass_arg = "(c)"
-        f.write(f"\tval io = IO(new {given_class_name}IO{pass_arg})\n")
+        f.write(f"\tval io = IO(new {fname_noext}IO{pass_arg})\n")
         f.write("}")
                 
           
